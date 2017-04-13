1928e23c191aa55a158ba4114a625144a46bead2
gasgit <glengardiner@gmail.com>
2017-04-13 01:14:42 +0100

    first commit data java

---
+++ src/ie/gasgit/ParseCSV.java
@@ -0,0 +1,45 @@
+package ie.gasgit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ParseCSV {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// create array list to store data
+		ArrayList<String> list = new ArrayList<>();
+		// read file with scanner
+		try (Scanner csvFile = new Scanner(new File("Galway_City_Beaches.csv"))) {
+			// iterate file and add to list
+			while (csvFile.hasNext()) {
+				list.add(csvFile.nextLine());
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// create temp array to store converted list
+		String[] tempArray = list.toArray(new String[1]);
+		String[][] csvArray = new String[tempArray.length][];
+		// 
+		for (int i = 0; i < csvArray.length; i++) {
+			csvArray[i] = tempArray[i].split(",");
+
+		}
+		// 
+		for (int i = 0; i < csvArray.length; i++) {
+			System.out.println(Arrays.toString(csvArray[i]));
+			
+
+			
+		}
+		
+
+	}
+}
